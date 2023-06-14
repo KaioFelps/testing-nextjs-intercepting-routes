f7019fa659377cc643f2a452f2f5d143f94299ab
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-06-14 16:30:01 -0300

    replaces modal html by modal component

--- src/app/feed/@modal/(...)stories/[id]/page.tsx
+++ src/app/feed/@modal/(...)stories/[id]/page.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import Modal from "./modal";
 
 interface IStoriesModal {
   params: {
@@ -6,24 +6,10 @@ interface IStoriesModal {
   };
 }
 
-interface IModal {
-  children: ReactNode;
-}
-
 export default function StoriesModal({ params: { id } }: IStoriesModal) {
   return (
     <Modal>
       <div>{id}</div>
     </Modal>
-  );
-}
-
-function Modal({ children }: IModal) {
-  return (
-    <div className="fixed inset-0 bg-black/50 backdrop:blur-3xl z-50">
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-items-center">
-        {children}
-      </div>
-    </div>
   );
 }
