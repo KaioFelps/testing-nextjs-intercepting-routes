095913362a999aa81d9b5306e5cb29377aa1cf20
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-06-14 16:43:22 -0300

    adds dismiss function on click on overlay

--- src/app/feed/@modal/(...)stories/[id]/modal.tsx
+++ src/app/feed/@modal/(...)stories/[id]/modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { ReactNode, useCallback, useEffect } from "react";
+import { ReactNode, useCallback, useEffect, useRef } from "react";
 
 interface IModal {
   children: ReactNode;
@@ -8,9 +8,10 @@ interface IModal {
 
 export default function Modal({ children }: IModal) {
   const { back } = useRouter();
+  const overlayRef = useRef<null | HTMLDivElement>(null);
+
   const onPressEsc = useCallback(
     (e: KeyboardEvent) => {
-      console.log(e);
       if (e.code === "Escape" || e.key === "Escape") {
         back();
       }
@@ -18,18 +19,39 @@ export default function Modal({ children }: IModal) {
     [back]
   );
 
+  const handleClickOnOverlay = useCallback(
+    (e: MouseEvent) => {
+      const eventTarget = e.target as HTMLDivElement | null;
+
+      if (eventTarget!.id === "modal_overlay") {
+        back();
+      }
+    },
+    [back]
+  );
+
   useEffect(() => {
+    const overlay = overlayRef.current;
     window.addEventListener("keydown", onPressEsc);
+    overlay?.addEventListener("click", handleClickOnOverlay);
 
     // cleanup function
-    return () => window.removeEventListener("keydown", onPressEsc);
-  }, [onPressEsc]);
+    return () => {
+      window.removeEventListener("keydown", onPressEsc);
+      overlay?.removeEventListener("click", handleClickOnOverlay);
+    };
+  }, [onPressEsc, handleClickOnOverlay]);
 
   return (
-    <div className="fixed inset-0 bg-black/50 backdrop:blur-3xl z-50">
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-items-center">
+    <>
+      <div
+        ref={overlayRef}
+        id="modal_overlay"
+        className="fixed inset-0 bg-black/50 backdrop:blur-3xl z-40"
+      ></div>
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid place-items-center z-50">
         {children}
       </div>
-    </div>
+    </>
   );
 }
